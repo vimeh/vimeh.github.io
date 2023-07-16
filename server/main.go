01d0784b5741b8a19d4c3dba8583bc30b9f95ca9
Vinay Mehta <vinaymehta.nyc@gmail.com>
2023-07-16 11:39:13 -0700

    hypermedia

--- server/main.go
+++ server/main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
-    "fmt"
     "net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, World!")
+// Take in a post request from the user, and log the string. If password matches
+// plain text of file line 1, then return file line 2 onwards
+func whats_the_password() {
 }
 
+func handler_index(w http.ResponseWriter, r *http.Request) {
+  http.ServeFile(w, r, "static/index.html")
+}
+
+func handler_rw(w http.ResponseWriter, r *http.Request) {
+  http.ServeFile(w, r, "static/red_wing.pdf")
+}
+
+
 func main() {
-    http.HandleFunc("/", handler)
+    http.HandleFunc("/", handler_index)
+    http.HandleFunc("/red_wing.pdf", handler_rw)
     http.ListenAndServe(":8001", nil)
 }
